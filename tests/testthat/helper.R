86cf1bedd0c71cdd7101c7008d50c744ef907597
Diana Garcia <dgarciac@broadinstitute.org>
2024-09-27 16:24:13 -0400

    Changing genes/sample arguments position in functions and tests, improving documentation

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -24,3 +24,19 @@ mocked_tpms <- matrix(runif(1000), nrow = 100, ncol = 10)
 rownames(mocked_tpms) <- paste0("gene", 1:100)
 colnames(mocked_tpms) <- paste0("sample", 1:10)
 
+mocked_tpms_min <- matrix(c(10, 1, 2, 5, 30,
+                15, 2, 3, 4, 15,
+                20, 3, 4, 3, 20,
+                25, 4, 5, 2, 25,
+                30, 5, 6, 1, 10), nrow = 5, byrow = TRUE)
+colnames(mocked_tpms_min) <- paste0("s", 1:5)
+rownames(mocked_tpms_min) <- paste0("g", 1:5)
+
+mocked_tpms_min_uq <- matrix(c(0.4, 0.25,  0.4, 1.25, 1.2,
+                   0.6, 0.50,  0.6, 1.00,  0.6,
+                   0.8, 0.75,  0.8, 0.75,  0.8,
+                   1.0, 1.00,  1.0, 0.50,  1.0,
+                   1.2, 1.25,  1.2, 0.25,  0.4), nrow = 5, byrow = T)
+colnames(mocked_tpms_min_uq) <- paste0("s", 1:5)
+rownames(mocked_tpms_min_uq) <- paste0("g", 1:5)
+
